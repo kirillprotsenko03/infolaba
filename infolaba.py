2fa77dc86d77dbb62405cb9dc0a0768cf7ab1e5c
kiril <kirill2003.pro2@gmail.com>
2021-12-04 22:00:58 +0300

    start of vk chat bot

--- infolaba.py
+++ infolaba.py
@@ -4,9 +4,14 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "hello, world"
+@app.route("/", methods=["GET", "POST"])
+def message_handle():
+    if request.method == "POST":
+        data = request.get_json()
+        if data["type"] == "confirmation" and data["group_id"] == 186539292:
+            return "51457a67"
+    else:
+        return "hello, world!"
 
 
 if __name__ == '__main__':
