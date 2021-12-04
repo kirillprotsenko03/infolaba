4c4879e87b9659aba6a9e39bbbeace78d4e90b82
kiril <kirill2003.pro2@gmail.com>
2021-12-04 21:09:07 +0300

    4 commit

--- infolaba.py
+++ infolaba.py
@@ -1,37 +1,13 @@
 import os
-import telebot
 from flask import Flask, request
 
-TOKEN = '15053356105:AAE7Qzd_4HfuMaiQ7KKmWgA0cvjlZ7vsz-g'
-APP_URL = f'https://https://infolaba.herokuapp.com//{TOKEN}'
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+app = Flask(__name__)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
-
-
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return '!', 200
-
-
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    return '!', 200
+@app.route("/")
+def hello_world():
+    return "hello, world"
 
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
