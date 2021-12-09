a2cf1b8da52c607bde3f72eef1f4d29caff02774
kiril <kirill2003.pro2@gmail.com>
2021-12-09 13:49:22 +0300

    crypto message

--- vk_functions.py
+++ vk_functions.py
@@ -13,13 +13,10 @@ def send_vk_message(data: dict, token: str):
         command = text[0]
         text = text[1:]
         message = ""
-        for word in text:
-            message += word
         if command == "закодируй":
-            message += ders_crypt(text)
-
+            message = command
         if command == "раскодируй":
-            message += decoding(text)
+            message = command
 
         api.messages.send(user_ids=user_id, message=message,
                           access_token=token, random_id=random.randint(-2147483648, 2147483647))
